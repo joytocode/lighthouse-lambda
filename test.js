59575ccfcb1c01e43c8908bd5b98ce7d5f07a1f3
Chau Nguyen <nmchau87@gmail.com>
2018-06-13 22:58:49 +0900

    feat v3: use chrome 67.0.3396.87 (#21)
    * feat: use chrome 67.0.3396.87

--- test.js
+++ test.js
@@ -1,7 +1,6 @@
 const path = require('path')
 const fs = require('fs')
 const createLighthouse = require('.')
-const { version } = require('./package.json')
 
 exports.handler = function (event, context, callback) {
   Promise.resolve()
@@ -10,14 +9,13 @@ exports.handler = function (event, context, callback) {
       return start()
         .then((results) => {
           if (event.saveResults) {
-            const filename = version.split('-')[0]
+            const filename = results.lhr.lighthouseVersion.split('-')[0]
             fs.writeFileSync(path.join(__dirname, `results/${filename}.json`), `${JSON.stringify(results.lhr, null, 2)}\n`)
             fs.writeFileSync(path.join(__dirname, `results/${filename}.html`), results.report)
           }
           return chrome.kill().then(() => callback(null, {
             userAgent: results.lhr.userAgent,
-            lighthouseVersion: results.lhr.lighthouseVersion,
-            lighthouseLambdaVersion: version
+            lighthouseVersion: results.lhr.lighthouseVersion
           }))
         })
         .catch((error) => {
